5d6cb97c7f4e8637816d84aa578403a62febb7de
Carson Mulligan <103183546+carsonmulligan@users.noreply.github.com>
2025-01-20 09:52:53 -0500

    f

---
+++ G_WoDApp.swift
@@ -0,0 +1,17 @@
+//
+//  G_WoDApp.swift
+//  G-WoD
+//
+//  Created by Carson Mulligan on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct G_WoDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
